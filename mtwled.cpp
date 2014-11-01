6948bc1747793445a3b3db5e743053c0b1d3e2bc
OhmEye <james@ohmeye.com>
2014-10-31 14:16:58 -1000

    Fix temp swing code

--- mtwled.cpp
+++ mtwled.cpp
@@ -84,6 +84,7 @@ void MTWLEDUpdate(patterncode pattern, unsigned long timer, int control) // send
 void MTWLEDLogic() // called from main loop
 {
   patterncode pattern = MTWLED_lastpattern;
+  int swing;
   
   if(MTWLED_control==1 || MTWLED_control==255) return;
   if(pattern.value==mtwled_nochange) return;
@@ -96,14 +97,13 @@ void MTWLEDLogic() // called from main loop
       pattern.value=MTWLEDConvert(mtwled_ready);
     MTWLEDUpdate(pattern);
   } else {
-    if(abs(degTargetHotend(0) - degHotend(0)) > MTWLED_swing) {
+    swing=abs(degTargetHotend(0) - degHotend(0));
+    if(swing < MTWLED_swing*2) {
       if(isHeatingHotend(0)) pattern.value=MTWLEDConvert(mtwled_templow);
       if(isCoolingHotend(0)) pattern.value=MTWLEDConvert(mtwled_temphigh);
+      if(swing < MTWLED_swing) pattern.value=MTWLEDConvert(mtwled_temphit);
       MTWLEDUpdate(pattern);
-    } else {
-      pattern.value=MTWLEDConvert(mtwled_temphit);
-      MTWLEDUpdate(pattern);
-    }
+    } 
   }
 }
 
@@ -111,7 +111,7 @@ void MTWLEDTemp() // called from inside heater function while heater is on to to
 {
 	byte pattern;
         if(MTWLED_control==255) return;
-	if(abs(degTargetHotend(0) - degHotend(0)) > MTWLED_swing) {
+	if(abs(degTargetHotend(0) - degHotend(0)) > MTWLED_swing*2) {
 	  pattern = 90 + ((degHotend(0) / degTargetHotend(0)) * 10);
 	  if(pattern > 99) pattern = 99;
 	  MTWLEDUpdate(pattern);
